8860c5a7be4d1c7185a6592d92651ae0072568f3
Pete Sword <pete.sword@yahoo.com>
2018-06-05 22:18:11 -0600

    some working tests

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Board, type: :model do
   describe 'attributes' do
     it "has a name" do
-      expect(Board).name.to eq("test")
+      expect(Board.name).to eq("Board")
     end
   end
 end
