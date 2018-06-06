44ead60ee67e71dd827d8c6099c353dda74657f2
Pete Sword <pete.sword@yahoo.com>
2018-06-05 23:05:07 -0600

    more tests

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -5,5 +5,17 @@ RSpec.describe Board, type: :model do
     it "has a name" do
       expect(Board.name).to eq("Board")
     end
+
+    it "has a valid factory" do
+      expect(FactoryBot.create(:board)).to be_valid
+    end
+
+    it "is invalid without a name" do
+      expect(FactoryBot.build(:board, name: nil)).to_not be_valid
+    end
+    
+    it "returns a board's name as a string" do
+    end
   end
+
 end
